690d41ccc478e18b62256e3c94bbc06dc1636c38
RUR <97384151+rur5996@users.noreply.github.com>
2023-02-26 11:35:20 +0100

    cleaned RobotMap

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -1,23 +1,9 @@
 package frc.robot;
 
-import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj2.command.button.JoystickButton;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
-import com.kauailabs.navx.frc.AHRS;
-import com.revrobotics.ColorSensorV3;
-
-import edu.wpi.first.wpilibj.AnalogInput;
-import edu.wpi.first.wpilibj.DigitalInput;
-import edu.wpi.first.wpilibj.I2C.Port;
-import edu.wpi.first.wpilibj.SPI;
-import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 
 
 public class RobotMap {
@@ -35,20 +21,4 @@ public class RobotMap {
     public static final WPI_TalonFX mover1 = new WPI_TalonFX(0);
     public static final WPI_TalonFX mover2 = new WPI_TalonFX(1);
     public static final WPI_VictorSPX gripper = new WPI_VictorSPX(2);
-
-    //TODO address joystick usage
-    public static Joystick logitech = new Joystick(1);
-
-    public static Trigger logitechTrigger = new JoystickButton(logitech, 1);
-    public static final Trigger logitechTwo = new JoystickButton(logitech, 2);
-    public static final Trigger logitechThree = new JoystickButton(logitech, 3);
-    public static final Trigger logitechFour = new JoystickButton(logitech, 4);
-    public static final Trigger logitechFive = new JoystickButton(logitech, 5);
-    public static final Trigger logitechSix = new JoystickButton(logitech, 6);
-    public static final Trigger logitechSeven = new JoystickButton(logitech, 7);
-    public static final Trigger logitechEight = new JoystickButton(logitech, 8);
-    public static final Trigger logitechNine = new JoystickButton(logitech, 9);
-    public static final Trigger logitechTen = new JoystickButton(logitech, 10);
-    public static final Trigger logitechEleven = new JoystickButton(logitech, 11);
-
 }
